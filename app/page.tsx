d94c63e165be82d610e07257f38bcad8e665f3c6
The73756 <the73756the@gmail.com>
2023-02-04 13:05:04 +0900

    add fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image';
-import LinkCard from '@/components/LinkCard';
-import SocialCard from '@/components/SocialCard';
 import { get } from '@vercel/edge-config';
 import { ILinksResponse } from '../types';
+import LinkCard from '@/components/LinkCard';
+import SocialCard from '@/components/SocialCard';
+import NotFound from '@/components/NotFound';
 
 export default async function Home() {
   const data = await get<ILinksResponse>('linktree');
 
-  if (!data) return <div>Not found</div>; //сделать фаллбек
+  if (!data) return <NotFound />;
 
   return (
     <div className="flex flex-col pt-20 items-center w-full px-8 align-center">
-      <Image className="rounded-full" src={data.avatar} width={100} height={100} alt={data.name} />
+      <Image className="rounded-full" src={data.avatar} width={90} height={90} alt={data.name} />
       <h1 className="font-semibold my-4 text-2xl text-white mb-5">{data.name}</h1>
       {data.links.map((link) => (
         <LinkCard {...link} key={link.url} />
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="h-full flex flex-col justify-center align-center">
+      <h1 className="text-white text-center text font-black text-6xl">Nothing</h1>
+      <p className="text-white text-center text-2xl mt-1 font-semibold">Maybe later...</p>
+    </div>
+  );
+};
+
+export default NotFound;
